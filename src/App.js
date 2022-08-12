a2c5f77f42bd965e8121b0dc4d34a6ec0c808036
woongsnote <woongsnote@gmail.com>
2022-08-12 12:38:35 +0900

    project init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [todoList, setList] = useState([
+    { id: 1, title: "react를 배워봅시다!" },
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="NewTodo">
+        <input
+          placeholder={"새로운 할 일을 입력햇!"}
+          type="text"
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+        />
+        <button
+          onClick={() =>
+            setList([...todoList, { id: todoList.length + 1, title: title }])
+          }>
+          추가하기
+        </button>
+      </div>
+      <h2>Todo List</h2>
+      <div className="TodoList">
+        {todoList.map((todo) => (
+          <div className="Todo" key={todo.id}>
+            {todo.title}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
